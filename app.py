1244161b93511f30f5a026cd0c3ddd1add6dfeca
Siddharths8212376 <siddharth8212376@gmail.com>
2020-03-11 23:28:33 +0530

    updated the web app

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-@app.route('/about')
-def about():
     return render_template('about.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 
 
