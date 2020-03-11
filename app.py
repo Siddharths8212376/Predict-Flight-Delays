466fb99af0c73a9eecc8965116e7b835c8ebdcf8
Sidharth Sunil <sidharth.sunil321@gmail.com>
2020-03-11 20:44:59 +0530

    added new pages in website

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 
 
 @app.route('/predict', methods=['POST'])
